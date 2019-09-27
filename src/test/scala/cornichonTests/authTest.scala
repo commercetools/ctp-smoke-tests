2cae25c894d892daf670232f7c58f83a07252d97
akovv <alexey.bityukov@commercetools.de>
2019-09-27 11:58:55 +0200

    refactor/ indentation

--- src/test/scala/cornichonTests/authTest.scala
+++ src/test/scala/cornichonTests/authTest.scala
@@ -1,12 +1,17 @@
 package cornichonTests
 import com.github.agourlay.cornichon.CornichonFeature
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.pureconfig._
+import eu.timepit.refined.string.Url
+import pureconfig.generic.auto._
+import pureconfig._
 
 class AuthTest extends CornichonFeature with FeatureConfig {
-  def feature = Feature("checking the API request"){
-    Scenario("API request returns 200"){
+  def feature = Feature("checking the API request") {
+    Scenario("API request returns 200") {
 
-      WithBasicAuth (clientId,clientSecret){
-        When I post(s"${authUrl}")
+      WithBasicAuth(clientId, clientSecret) {
+        When I post(authUrl)
         .withParams(
           "grant_type" -> "client_credentials",
           "scope" -> s"manage_project:${projectKey}")
