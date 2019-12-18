844395470854e8058f35460297f6ba696c13f39b
akovv <alexey.bityukov@commercetools.de>
2019-12-18 15:12:47 +0100

    WIP ScenarioResourceStep set up

--- src/test/scala/cornichonTests/CheckoutExistProdTest.scala
+++ src/test/scala/cornichonTests/CheckoutExistProdTest.scala
@@ -1,91 +1,98 @@
 package cornichonTests
 import com.github.agourlay.cornichon.CornichonFeature
 import com.github.agourlay.cornichon.core.FeatureDef
+import com.github.agourlay.cornichon.http.{HttpRequest, RootExtractor}
+import com.github.agourlay.cornichon.resolver.JsonMapper
+import com.github.agourlay.cornichon.steps.regular.EffectStep
+import com.github.agourlay.cornichon.steps.wrapped.ScenarioResourceStep
 
-class CheckoutExistProdTest extends AddProduct {
+class CheckoutExistProdTest extends CornichonFeature with FeatureWithProduct {
+
   override def feature: FeatureDef = Feature("Checkout process for existing product") {
     Scenario("Add product | Create order | Cleanup") {
-      Then assert session_value("orderFromCartId").isPresent
+        Then assert addTaxCategoryStep
+
+        And I show_last_body_json
+        And I show_session
     }
   }
+
 }
 
-trait AddProduct extends CheckoutTest with CornichonFeature with FeatureWithToken with FeatureWithProduct {
-  override lazy val baseUrl = apiUrl + s"/$projectKey"
+//trait AddProduct extends CheckoutTest {
 
-  beforeEachScenario(
-    Attach{
-      WithToken {
-        createTaxCategory
-        addProductType
-        addProduct
-        createCart
-        addLineItem
-        createOrderFromCart
-        getOrderFromCart
-      }
-    }
-  )
+//  beforeEachScenario(
+//    Attach{
+//      WithToken {
+//        createTaxCategory
+//        addProductType
+//        addProduct
+//        createCart
+//        addLineItem
+//        createOrderFromCart
+//        getOrderFromCart
+//      }
+//    }
+//  )
+//
+//  afterEachScenario(
+//    Attach{
+//      WithToken {
+//        getCartById
+//        deleteOrderFromCart
+//        deleteCart
+//        unpublishProduct
+//        deleteProduct
+//        deleteProductType
+//      }
+//    }
+//  )
 
-  afterEachScenario(
-    Attach{
-      WithToken {
-        getCartById
-        deleteOrderFromCart
-        deleteCart
-        unpublishProduct
-        deleteProduct
-        deleteProductType
-      }
-    }
-  )
-
-  def addLineItem =
-    Attach {
-      When I post("/carts/<cartId>").withBody("""
-        |{
-        |   "version": <cartVersion>,
-        |   "actions": [
-        |     {
-        |       "action" : "addLineItem",
-        |       "productId" : "<productId>"
-        |     },
-        |     {
-        |       "action" : "setShippingAddress",
-        |       "address": {
-        |         "key": "test_address",
-        |         "company": "commercetools GmbH",
-        |         "streetName": "Sonnenallee",
-        |         "streetNumber": "223",
-        |         "city": "Berlin",
-        |         "postalCode": "12059",
-        |         "country": "DE"
-        |       }
-        |     }
-        |   ]
-        |}
-        |""".stripMargin)
-      Then I save_body_path("version" -> "cartVersion")
-      And assert status.is(200)
-    }
-
-  override def createOrderFromCart =
-    Attach {
-      When I post("/orders").withBody("""
-           |{
-           |   "id" : "<cartId>",
-           |   "version" : <cartVersion>
-           |}
-           |""".stripMargin)
-      Then I save_body_path("id" -> "orderFromCartId")
-      Then I save_body_path("version" -> "orderFromCartVersion")
-      Then assert status.is(201)
-    }
-
-  def getOrderFromCart =
-    Attach {
-      When I get("/orders/<orderFromCartId>")
-      Then assert status.is(200)
-    }
-}
+//  def addLineItem =
+//    Attach {
+//      When I post("/carts/<cartId>").withBody("""
+//        |{
+//        |   "version": <cartVersion>,
+//        |   "actions": [
+//        |     {
+//        |       "action" : "addLineItem",
+//        |       "productId" : "<productId>"
+//        |     },
+//        |     {
+//        |       "action" : "setShippingAddress",
+//        |       "address": {
+//        |         "key": "test_address",
+//        |         "company": "commercetools GmbH",
+//        |         "streetName": "Sonnenallee",
+//        |         "streetNumber": "223",
+//        |         "city": "Berlin",
+//        |         "postalCode": "12059",
+//        |         "country": "DE"
+//        |       }
+//        |     }
+//        |   ]
+//        |}
+//        |""".stripMargin)
+//      Then I save_body_path("version" -> "cartVersion")
+//      And assert status.is(200)
+//    }
+//
+//  override def createOrderFromCart =
+//    Attach {
+//      When I post("/orders").withBody("""
+//           |{
+//           |   "id" : "<cartId>",
+//           |   "version" : <cartVersion>
+//           |}
+//           |""".stripMargin)
+//      Then I save_body_path("id" -> "orderFromCartId")
+//      Then I save_body_path("version" -> "orderFromCartVersion")
+//      Then assert status.is(201)
+//    }
+//
+//  def getOrderFromCart =
+//    Attach {
+//      When I get("/orders/<orderFromCartId>")
+//      Then assert status.is(200)
+//    }
 
--- src/test/scala/cornichonTests/FeatureWithProduct.scala
+++ src/test/scala/cornichonTests/FeatureWithProduct.scala
@@ -1,106 +1,164 @@
 package cornichonTests
 
 import com.github.agourlay.cornichon.CornichonFeature
+import com.github.agourlay.cornichon.http.{HttpRequest, RootExtractor}
+import com.github.agourlay.cornichon.resolver.{JsonMapper, Mapper}
+import com.github.agourlay.cornichon.steps.regular.EffectStep
+import com.github.agourlay.cornichon.steps.wrapped.ScenarioResourceStep
 
-trait FeatureWithProduct {
-  this: CornichonFeature =>
+trait FeatureWithProduct extends FeatureWithToken {
+  this: CornichonFeature with FeatureConfig =>
 
-  def addProductType=
+  override def registerExtractors: Map[String, Mapper] = FeatureWithProduct.extractors ++ AuthSteps.registerExtractors
+
+  lazy val baseUrlHttp = httpServiceByURL(s"$apiUrl" + s"/$projectKey")
+
+  def addTaxCategoryStep =
     Attach {
-      When I post("/product-types").withBody(
-        """
-          |{
-          |   "name": "test_product_type",
-          |   "description": "Test product type.",
-          |   "attributes": [
-          |     {
-          |       "name": "some_attribute_name",
-          |       "type": {
-          |       "name": "text"
-          |       },
-          |       "isRequired": false,
-          |       "isSearchable": true,
-          |       "label": {
-          |       "en": "some label"
-          |       },
-          |       "attributeConstraint": "None"
-          |     }
-          |   ]
-          |}
-          |""".stripMargin
-      )
-      Then I save_body_path("id" -> "productTypeId", "version" -> "productTypeVersion")
-      Then assert status.is(201)
+      WithToken {
+        ScenarioResourceStep(
+          title = "add tax cat",
+          acquire =  addTaxCategory,
+          release = deleteTaxCategory
+        )
+      }
     }
 
-  def addProduct =
-    Attach {
-      When I post("/products").withBody(
-        """
-          |{
-          |   "productType" : {
-          |     "id" : "<productTypeId>",
-          |     "typeId" : "product-type"
-          |   },
-          |   "name" : {
-          |    "en" : "Some Product"
-          |   },
-          |   "slug" : {
-          |     "en" : "product_slug_<random-positive-integer>"
-          |   },
-          |   "publish": true,
-          |   "masterVariant" : {
-          |     "sku" : "SKU-<random-positive-integer>",
-          |     "prices" : [
-          |       {
-          |         "value" : {
-          |           "type": "centPrecision",
-          |           "currencyCode" : "EUR",
-          |           "centAmount" : 4200
-          |         }
-          |       }
-          |     ]
-          |   },
-          |   "taxCategory" : {
-          |     "typeId" : "tax-category",
-          |     "id" : "<taxCategoryId>"
-          |   }
-          |}
-          |""".stripMargin
-      )
-      Then I save_body_path("id" -> "productId", "version" -> "productVersion")
-      Then assert status.is(201)
-    }
+  def addTaxCategory = EffectStep(
+    title = "create tax category",
+    effect = baseUrlHttp.requestEffect(
+      request = HttpRequest
+        .post("/tax-categories")
+        .withBody(
+          """
+            |{
+            |  "name" : "test-tax-category-<random-positive-integer>",
+            |  "rates" : [ {
+            |    "name" : "test-tax-category",
+            |    "amount" : 0.2,
+            |    "includedInPrice" : true,
+            |    "country" : "DE"
+            |  } ]
+            |}
+            |""".stripMargin),
+      extractor = RootExtractor("tax-category"),
+      expectedStatus = Some(201)
+    )
+  )
 
-  def unpublishProduct =
-    Attach {
-      When I post("/products/<productId>").withBody(
-        """
-          |{
-          |   "version": <productVersion>,
-          |   "actions": [
-          |     {
-          |       "action" : "unpublish"
-          |     }
-          |   ]
-          |}
-          |""".stripMargin)
-
-      Then I save_body_path("version" -> "productVersion")
-    }
-
-  def deleteProduct =
-    Attach {
-      When I delete("/products/<productId>").
-        withParams("version" -> "<productVersion>")
-      Then assert status.is(200)
-    }
-
-  def deleteProductType =
-    Attach {
-      When I delete("/product-types/<productTypeId>").
-        withParams("version" -> "<productTypeVersion>")
-      Then assert status.is(200)
-    }
+  def deleteTaxCategory = EffectStep(
+    title = "deleting tax category",
+    effect = baseUrlHttp.requestEffect(
+      request = HttpRequest
+        .delete(s"/tax-categories/<tax-category-id>")
+        .withParams("version" -> s"<tax-category-version>"),
+      expectedStatus = Some(200)
+    )
+  )
 }
+
+object FeatureWithProduct {
+  val extractors = Map(
+    "tax-category-id" -> JsonMapper("tax-category", "id"),
+    "tax-category-version" -> JsonMapper("tax-category", "version")
+  )
+}
+
+//  def addProductType=
+//    Attach {
+//      When I post("/product-types").withBody(
+//        """
+//          |{
+//          |   "name": "test_product_type",
+//          |   "description": "Test product type.",
+//          |   "attributes": [
+//          |     {
+//          |       "name": "some_attribute_name",
+//          |       "type": {
+//          |       "name": "text"
+//          |       },
+//          |       "isRequired": false,
+//          |       "isSearchable": true,
+//          |       "label": {
+//          |       "en": "some label"
+//          |       },
+//          |       "attributeConstraint": "None"
+//          |     }
+//          |   ]
+//          |}
+//          |""".stripMargin
+//      )
+//      Then I save_body_path("id" -> "productTypeId", "version" -> "productTypeVersion")
+//      Then assert status.is(201)
+//    }
+//
+//  def addProduct =
+//    Attach {
+//      When I post("/products").withBody(
+//        """
+//          |{
+//          |   "productType" : {
+//          |     "id" : "<productTypeId>",
+//          |     "typeId" : "product-type"
+//          |   },
+//          |   "name" : {
+//          |    "en" : "Some Product"
+//          |   },
+//          |   "slug" : {
+//          |     "en" : "product_slug_<random-positive-integer>"
+//          |   },
+//          |   "publish": true,
+//          |   "masterVariant" : {
+//          |     "sku" : "SKU-<random-positive-integer>",
+//          |     "prices" : [
+//          |       {
+//          |         "value" : {
+//          |           "type": "centPrecision",
+//          |           "currencyCode" : "EUR",
+//          |           "centAmount" : 4200
+//          |         }
+//          |       }
+//          |     ]
+//          |   },
+//          |   "taxCategory" : {
+//          |     "typeId" : "tax-category",
+//          |     "id" : "<taxCategoryId>"
+//          |   }
+//          |}
+//          |""".stripMargin
+//      )
+//      Then I save_body_path("id" -> "productId", "version" -> "productVersion")
+//      Then assert status.is(201)
+//    }
+//
+//  def unpublishProduct =
+//    Attach {
+//      When I post("/products/<productId>").withBody(
+//        """
+//          |{
+//          |   "version": <productVersion>,
+//          |   "actions": [
+//          |     {
+//          |       "action" : "unpublish"
+//          |     }
+//          |   ]
+//          |}
+//          |""".stripMargin)
+//
+//      Then I save_body_path("version" -> "productVersion")
+//    }
+//
+//  def deleteProduct =
+//    Attach {
+//      When I delete("/products/<productId>").
+//        withParams("version" -> "<productVersion>")
+//      Then assert status.is(200)
+//    }
+//
+//  def deleteProductType =
+//    Attach {
+//      When I delete("/product-types/<productTypeId>").
+//        withParams("version" -> "<productTypeVersion>")
+//      Then assert status.is(200)
+//    }
 
