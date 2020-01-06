c0ecdfdf980c29479b202c6144c571dfacb6d997
akovv <alexey.bityukov@commercetools.de>
2020-01-06 14:51:40 +0100

    scenarioResourceStep set up

--- src/test/scala/cornichonTests/CheckoutExistProdTest.scala
+++ src/test/scala/cornichonTests/CheckoutExistProdTest.scala
@@ -1,98 +1,21 @@
 package cornichonTests
 import com.github.agourlay.cornichon.CornichonFeature
 import com.github.agourlay.cornichon.core.FeatureDef
-import com.github.agourlay.cornichon.http.{HttpRequest, RootExtractor}
-import com.github.agourlay.cornichon.resolver.JsonMapper
-import com.github.agourlay.cornichon.steps.regular.EffectStep
-import com.github.agourlay.cornichon.steps.wrapped.ScenarioResourceStep
 
 class CheckoutExistProdTest extends CornichonFeature with FeatureWithProduct {
 
   override def feature: FeatureDef = Feature("Checkout process for existing product") {
-    Scenario("Add product | Create order | Cleanup") {
+    Scenario("Add product | Add LineItem | Create order") {
       Given a newTaxCategory
-      When I queryTaxCategories
-      Then assert status.is(200)
-      And I show_last_body_json
+      Given a newProductType
+      Given a newProduct
+      Given a newCart
+      When  I addLineItem
+      And   I createOrderFromCart
+      And   I getOrderFromCart
+      And   I getCartById
+      Then  assert status.is(200)
     }
   }
-
 }
-
-//trait AddProduct extends CheckoutTest {
-
-//  beforeEachScenario(
-//    Attach{
-//      WithToken {
-//        createTaxCategory
-//        addProductType
-//        addProduct
-//        createCart
-//        addLineItem
-//        createOrderFromCart
-//        getOrderFromCart
-//      }
-//    }
-//  )
-//
-//  afterEachScenario(
-//    Attach{
-//      WithToken {
-//        getCartById
-//        deleteOrderFromCart
-//        deleteCart
-//        unpublishProduct
-//        deleteProduct
-//        deleteProductType
-//      }
-//    }
-//  )
-
-//  def addLineItem =
-//    Attach {
-//      When I post("/carts/<cartId>").withBody("""
-//        |{
-//        |   "version": <cartVersion>,
-//        |   "actions": [
-//        |     {
-//        |       "action" : "addLineItem",
-//        |       "productId" : "<productId>"
-//        |     },
-//        |     {
-//        |       "action" : "setShippingAddress",
-//        |       "address": {
-//        |         "key": "test_address",
-//        |         "company": "commercetools GmbH",
-//        |         "streetName": "Sonnenallee",
-//        |         "streetNumber": "223",
-//        |         "city": "Berlin",
-//        |         "postalCode": "12059",
-//        |         "country": "DE"
-//        |       }
-//        |     }
-//        |   ]
-//        |}
-//        |""".stripMargin)
-//      Then I save_body_path("version" -> "cartVersion")
-//      And assert status.is(200)
-//    }
-//
-//  override def createOrderFromCart =
-//    Attach {
-//      When I post("/orders").withBody("""
-//           |{
-//           |   "id" : "<cartId>",
-//           |   "version" : <cartVersion>
-//           |}
-//           |""".stripMargin)
-//      Then I save_body_path("id" -> "orderFromCartId")
-//      Then I save_body_path("version" -> "orderFromCartVersion")
-//      Then assert status.is(201)
-//    }
-//
-//  def getOrderFromCart =
-//    Attach {
-//      When I get("/orders/<orderFromCartId>")
-//      Then assert status.is(200)
-//    }
 
--- src/test/scala/cornichonTests/FeatureWithProduct.scala
+++ src/test/scala/cornichonTests/FeatureWithProduct.scala
@@ -19,14 +19,37 @@ trait FeatureWithProduct extends FeatureWithToken {
     release = deleteTaxCategory
   )
 
+  def newProductType = ScenarioResourceStep(
+    title = "add new product type",
+    acquire = addProductType,
+    release = deleteProductType
+  )
+
+  def newProduct = ScenarioResourceStep(
+    title = "add new product",
+    acquire = addProduct,
+    release = deleteProduct
+  )
+
+  def newCart = ScenarioResourceStep(
+    title = "add new cart",
+    acquire = createCart,
+    release = deleteCart
+  )
+
+  def createOrderFromCart = ScenarioResourceStep(
+    title = "create order from cart",
+    acquire = createOrder,
+    release = deleteOrder
+  )
+
   def queryTaxCategories =
     Attach {
-      WithToken{
+      WithToken {
         EffectStep(
           title = "query tax cats",
           effect = baseUrlHttp.requestEffect(
-            request = HttpRequest
-                .get("/tax-categories"),
+            request = HttpRequest.get("/tax-categories"),
             expectedStatus = Some(200)
           )
         )
@@ -41,8 +64,7 @@ trait FeatureWithProduct extends FeatureWithToken {
           effect = baseUrlHttp.requestEffect(
             request = HttpRequest
               .post("/tax-categories")
-              .withBody(
-                """
+              .withBody("""
                   |{
                   |  "name" : "test-tax-category-<random-positive-integer>",
                   |  "rates" : [ {
@@ -75,110 +97,301 @@ trait FeatureWithProduct extends FeatureWithToken {
       }
     }
 
-  object FeatureWithProduct {
-    val extractors = Map(
-      "tax-category-id" -> JsonMapper("tax-category", "id"),
-      "tax-category-version" -> JsonMapper("tax-category", "version")
-    )
-  }
+  def addProductType =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "adding type category",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .post("/product-types")
+              .withBody("""
+                    |{
+                    |    "name": "test_product_type",
+                    |    "description": "Test product type.",
+                    |    "attributes": [
+                    |        {
+                    |            "name": "some_attribute_name",
+                    |            "type": {
+                    |                "name": "text"
+                    |            },
+                    |            "isRequired": false,
+                    |            "isSearchable": true,
+                    |            "label": {
+                    |                "en": "some label"
+                    |            },
+                    |            "attributeConstraint": "None"
+                    |        }
+                    |    ]
+                    |}
+                    |""".stripMargin),
+            extractor = RootExtractor("product-type"),
+            expectedStatus = Some(201)
+          )
+        )
+      }
+    }
+
+  def deleteProductType =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "deleting product type",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .delete(s"/product-types/<product-type-id>")
+              .withParams("version" -> s"<product-type-version>"),
+            expectedStatus = Some(200)
+          )
+        )
+      }
+    }
+
+  def addProduct =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "adding product",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .post("/products")
+              .withBody("""
+                  |{
+                  |  "productType" : {
+                  |    "id" : "<product-type-id>",
+                  |    "typeId" : "product-type"
+                  |  },
+                  |  "name" : {
+                  |    "en" : "Test Product"
+                  |  },
+                  |  "slug" : {
+                  |    "en" : "product_slug_<random-uuid>"
+                  |  },
+                  |  "publish" : true,
+                  |  "masterVariant" : {
+                  |     "sku" : "SKU-<random-positive-integer>",
+                  |     "prices" : [
+                  |       {
+                  |         "value" : {
+                  |           "type": "centPrecision",
+                  |           "currencyCode" : "EUR",
+                  |           "centAmount" : 4200
+                  |         }
+                  |       }
+                  |     ]
+                  |  },
+                  |  "taxCategory" : {
+                  |     "typeId" : "tax-category",
+                  |     "id" : "<tax-category-id>"
+                  |  }
+                  |}
+                  |""".stripMargin),
+            extractor = RootExtractor("product"),
+            expectedStatus = Some(201)
+          )
+        )
+      }
+    }
+
+  def unpublishProduct =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "unpublishing product",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .post(s"/products/<product-id>")
+              .withBody("""
+                  |{
+                  |   "version": <product-version>,
+                  |   "actions": [
+                  |     {
+                  |       "action" : "unpublish"
+                  |     }
+                  |   ]
+                  |}
+                  |""".stripMargin),
+            extractor = RootExtractor("product"),
+            expectedStatus = Some(200)
+          )
+        )
+      }
+    }
+
+  def deleteProductById =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "deleting product",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .delete(s"/products/<product-id>")
+              .withParams("version" -> s"<product-version>"),
+            expectedStatus = Some(200)
+          )
+        )
+      }
+    }
+
+  def deleteProduct =
+    Attach {
+      unpublishProduct
+      deleteProductById
+    }
+
+  def createCart =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "create cart",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .post("/carts")
+              .withBody("""
+                  |{
+                  |  "currency" : "EUR",
+                  |  "shippingAddress" : {
+                  |  	"country" : "DE"
+                  |  }
+                  |}
+                  |""".stripMargin),
+            extractor = RootExtractor("cart"),
+            expectedStatus = Some(201)
+          )
+        )
+      }
+    }
+
+  def deleteCart =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "deleting cart",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .delete(s"/carts/<cart-id>")
+              .withParams("version" -> s"<cart-version>"),
+            expectedStatus = Some(200)
+          )
+        )
+      }
+    }
+
+  def addLineItem =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "adding line item",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .post("/carts/<cart-id>")
+              .withBody("""
+                  |{
+                  |   "version": <cart-version>,
+                  |   "actions": [
+                  |     {
+                  |       "action" : "addLineItem",
+                  |       "productId" : "<product-id>"
+                  |     },
+                  |     {
+                  |       "action" : "setShippingAddress",
+                  |       "address": {
+                  |         "key": "test_address",
+                  |         "company": "commercetools GmbH",
+                  |         "streetName": "Sonnenallee",
+                  |         "streetNumber": "223",
+                  |         "city": "Berlin",
+                  |         "postalCode": "12059",
+                  |         "country": "DE"
+                  |       }
+                  |     }
+                  |   ]
+                  |}
+                  |""".stripMargin),
+            extractor = RootExtractor("cart"),
+            expectedStatus = Some(200)
+          )
+        )
+      }
+    }
+
+  def createOrder =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "create order",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .post("/orders")
+              .withBody("""
+                |{
+                |   "id" : "<cart-id>",
+                |   "version" : <cart-version>
+                |}
+                |""".stripMargin),
+            extractor = RootExtractor("order"),
+            expectedStatus = Some(201)
+          )
+        )
+      }
+    }
+
+  def deleteOrder =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "delete order",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest
+              .delete("/orders/<order-id>")
+              .withParams("version" -> "<order-version>"),
+            expectedStatus = Some(200)
+          )
+        )
+      }
+    }
+
+  def getOrderFromCart =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "query order by id",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest.get("/orders/<order-id>"),
+            expectedStatus = Some(200)
+          )
+        )
+      }
+    }
+
+  def getCartById =
+    Attach {
+      WithToken {
+        EffectStep(
+          title = "query cart by id",
+          effect = baseUrlHttp.requestEffect(
+            request = HttpRequest.get("/carts/<cart-id>"),
+            extractor = RootExtractor("cart"),
+            expectedStatus = Some(200)
+          )
+        )
+      }
+    }
 
 }
 
-//  def addProductType=
-//    Attach {
-//      When I post("/product-types").withBody(
-//        """
-//          |{
-//          |   "name": "test_product_type",
-//          |   "description": "Test product type.",
-//          |   "attributes": [
-//          |     {
-//          |       "name": "some_attribute_name",
-//          |       "type": {
-//          |       "name": "text"
-//          |       },
-//          |       "isRequired": false,
-//          |       "isSearchable": true,
-//          |       "label": {
-//          |       "en": "some label"
-//          |       },
-//          |       "attributeConstraint": "None"
-//          |     }
-//          |   ]
-//          |}
-//          |""".stripMargin
-//      )
-//      Then I save_body_path("id" -> "productTypeId", "version" -> "productTypeVersion")
-//      Then assert status.is(201)
-//    }
-//
-//  def addProduct =
-//    Attach {
-//      When I post("/products").withBody(
-//        """
-//          |{
-//          |   "productType" : {
-//          |     "id" : "<productTypeId>",
-//          |     "typeId" : "product-type"
-//          |   },
-//          |   "name" : {
-//          |    "en" : "Some Product"
-//          |   },
-//          |   "slug" : {
-//          |     "en" : "product_slug_<random-positive-integer>"
-//          |   },
-//          |   "publish": true,
-//          |   "masterVariant" : {
-//          |     "sku" : "SKU-<random-positive-integer>",
-//          |     "prices" : [
-//          |       {
-//          |         "value" : {
-//          |           "type": "centPrecision",
-//          |           "currencyCode" : "EUR",
-//          |           "centAmount" : 4200
-//          |         }
-//          |       }
-//          |     ]
-//          |   },
-//          |   "taxCategory" : {
-//          |     "typeId" : "tax-category",
-//          |     "id" : "<taxCategoryId>"
-//          |   }
-//          |}
-//          |""".stripMargin
-//      )
-//      Then I save_body_path("id" -> "productId", "version" -> "productVersion")
-//      Then assert status.is(201)
-//    }
-//
-//  def unpublishProduct =
-//    Attach {
-//      When I post("/products/<productId>").withBody(
-//        """
-//          |{
-//          |   "version": <productVersion>,
-//          |   "actions": [
-//          |     {
-//          |       "action" : "unpublish"
-//          |     }
-//          |   ]
-//          |}
-//          |""".stripMargin)
-//
-//      Then I save_body_path("version" -> "productVersion")
-//    }
-//
-//  def deleteProduct =
-//    Attach {
-//      When I delete("/products/<productId>").
-//        withParams("version" -> "<productVersion>")
-//      Then assert status.is(200)
-//    }
-//
-//  def deleteProductType =
-//    Attach {
-//      When I delete("/product-types/<productTypeId>").
-//        withParams("version" -> "<productTypeVersion>")
-//      Then assert status.is(200)
-//    }
+object FeatureWithProduct {
 
+  val extractors = Map(
+    "tax-category-id"      -> JsonMapper("tax-category", "id"),
+    "tax-category-version" -> JsonMapper("tax-category", "version"),
+    "product-type-id"      -> JsonMapper("product-type", "id"),
+    "product-type-version" -> JsonMapper("product-type", "version"),
+    "product-id"           -> JsonMapper("product", "id"),
+    "product-version"      -> JsonMapper("product", "version"),
+    "cart-id"              -> JsonMapper("cart", "id"),
+    "cart-version"         -> JsonMapper("cart", "version"),
+    "order-id"             -> JsonMapper("order", "id"),
+    "order-version"        -> JsonMapper("order", "version")
+  )
+}
