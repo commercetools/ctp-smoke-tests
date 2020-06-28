a049c96658c28dd482675946ec780d078ec36ecc
Scala steward CTP <automation@commercetools.de>
2020-06-28 02:01:34 +0000

    Update pureconfig to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,6 @@ object Dependencies {
       "com.github.agourlay"   %% "cornichon-test-framework" % "0.19.1" ::
       "eu.timepit"            %% "refined"                  % "0.9.14" ::
       "eu.timepit"            %% "refined-pureconfig"       % "0.9.14" ::
-      "com.github.pureconfig" %% "pureconfig"               % "0.12.3" ::
+      "com.github.pureconfig" %% "pureconfig"               % "0.13.0" ::
       Nil map (_ % Test)
 }
