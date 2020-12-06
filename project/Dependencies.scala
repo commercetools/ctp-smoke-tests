9116000375ee281cbf36926d95565f85d8138bf2
Scala steward CTP <automation@commercetools.de>
2020-12-06 02:01:40 +0000

    Update refined, refined-pureconfig to 0.9.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
 
   lazy val smokeTests =
       "com.github.agourlay"   %% "cornichon-test-framework" % "0.19.5" ::
-      "eu.timepit"            %% "refined"                  % "0.9.18" ::
-      "eu.timepit"            %% "refined-pureconfig"       % "0.9.18" ::
+      "eu.timepit"            %% "refined"                  % "0.9.19" ::
+      "eu.timepit"            %% "refined-pureconfig"       % "0.9.19" ::
       "com.github.pureconfig" %% "pureconfig"               % "0.14.0" ::
       "com.github.pureconfig" %% "pureconfig"               % "0.13.0" ::
       Nil map (_ % Test)
