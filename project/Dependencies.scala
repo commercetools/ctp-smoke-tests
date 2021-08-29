badfd841b2aee54b866af10a54f8835d43ad7dd9
Scala steward CTP <automation@commercetools.de>
2021-08-29 02:01:44 +0000

    Reformat with scalafmt 3.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,10 @@ import sbt._
 object Dependencies {
 
   lazy val smokeTests =
-    "com.github.agourlay"     %% "cornichon-test-framework" % "0.19.8" ::
-      "eu.timepit"            %% "refined"                  % "0.9.27" ::
-      "eu.timepit"            %% "refined-pureconfig"       % "0.9.27" ::
-      "com.github.pureconfig" %% "pureconfig"               % "0.16.0" ::
-      "com.github.pureconfig" %% "pureconfig"               % "0.16.0" ::
-      Nil map (_               % Test)
+    "com.github.agourlay" %% "cornichon-test-framework" % "0.19.8" ::
+      "eu.timepit" %% "refined" % "0.9.27" ::
+      "eu.timepit" %% "refined-pureconfig" % "0.9.27" ::
+      "com.github.pureconfig" %% "pureconfig" % "0.16.0" ::
+      "com.github.pureconfig" %% "pureconfig" % "0.16.0" ::
+      Nil map (_ % Test)
 }
