9dcd4b5ec86287dae9ba1dcfa52b46dff14a0372
Scala steward CTP <automation@commercetools.de>
2022-12-04 02:00:57 +0000

    Update cornichon-test-framework to 0.20.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   lazy val smokeTests =
-    "com.github.agourlay" %% "cornichon-test-framework" % "0.20.4" ::
+    "com.github.agourlay" %% "cornichon-test-framework" % "0.20.5" ::
       "eu.timepit" %% "refined" % "0.10.1" ::
       "eu.timepit" %% "refined-pureconfig" % "0.10.1" ::
       "com.github.pureconfig" %% "pureconfig" % "0.17.2" ::
